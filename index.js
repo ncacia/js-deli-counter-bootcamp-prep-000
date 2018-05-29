23c75b4a5257b975833807a9ed151115c315424b
Nicole Tams <nicole.cacia@gmail.com>
2018-05-29 10:58:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ else {
   for (var i=0; i < katzDeliLine.length; i++){
   
   var n=i+1;
-  otherLine.push (" "+n+"." +katzDeliLine[i]);
+  otherLine.push (" "+n+ "." + katzDeliLine[i]);
   }
   return ("The Line is currently: " + otherLine);
 }
